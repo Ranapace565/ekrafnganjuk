751398263863534f70da74408a783addd7dbded7
Ranapace565 <Ranapace565@users.noreply.github.com>
2025-06-19 10:51:10 +0700

    artikel

--- resources/views/main-visitor/article-detail/body.blade.php
+++ resources/views/main-visitor/article-detail/body.blade.php
@@ -1,3 +1,45 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+<section class="bg-white dark:bg-gray-900 sm:px-6">
+    <div class="max-w-screen-xl py-4 mx-auto">
+
+        {{-- tampilan artikel --}}
+        <div class="mb-6 lg:mb-0 lg:mt-0 mt-5 lg:col-span-9 ">
+            <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+                <div class="flex justify-between mx-auto max-w-screen-xl ">
+                    <article class=" w-full format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                        @include('main-visitor.article-detail.header')
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla sint in eligendi,
+                            expedita odit exercitationem
+                            provident ut explicabo iure quasi alias quaerat illo dignissimos obcaecati, quos sed maxime
+                            velit cum! Totam alias
+                            dolorem fugiat quaerat corrupti accusamus repellendus accusantium, libero molestias
+                            perspiciatis, error eum odit
+                            nihil itaque quae nesciunt laborum! Odit necessitatibus consequatur quod maxime est
+                            quibusdam culpa, deserunt enim
+                            commodi vel ipsum aut harum alias nihil quasi temporibus officiis eum delectus ullam?
+                            Accusantium modi iusto
+                            mollitia! Eligendi repellat ipsa, eveniet aspernatur voluptate inventore hic minus
+                            dignissimos quidem temporibus
+                            modi porro nostrum laboriosam maxime voluptas quod pariatur laudantium ipsum nemo est
+                            facilis architecto non error
+                            voluptates. Vitae odit repellat ipsam deserunt ad esse a soluta reiciendis, cum dignissimos
+                            sint magni maxime alias
+                            voluptatibus aliquam fugit beatae praesentium dicta accusamus pariatur nemo libero numquam
+                            culpa! Reprehenderit
+                            doloribus sit impedit quo accusamus. Debitis quae odio asperiores, magni illum nostrum
+                            voluptas? Tempore provident
+                            quas itaque, ullam ratione officiis magnam soluta corrupti iste saepe debitis labore
+                            explicabo nam sit ducimus
+                            mollitia dolorum vero quos. Exercitationem eos in, dicta quidem nemo a quos maiores
+                            explicabo dignissimos.
+                            Explicabo, voluptatem alias temporibus veritatis laudantium dignissimos dolorem! Rem, sit
+                            vitae culpa cupiditate
+                            quibusdam sint voluptate fuga dolor?
+                        </p>
+
+                    </article>
+                </div>
+            </main>
+        </div>
+    </div>
+</section>
--- resources/views/main-visitor/article-detail/footer.blade.php
+++ resources/views/main-visitor/article-detail/footer.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+<aside aria-label="Related articles" class="py-8 lg:py-24 bg-gray-50 dark:bg-gray-800">
+    <div class="px-4 mx-auto max-w-screen-xl">
+        <h2 class="mb-8 text-2xl font-bold text-gray-900 dark:text-white">Berita Terkini</h2>
+        <div class="flex flex-row gap-6 overflow-x-auto px-2 pb-6 dark:dark scrollbar-custom ">
+            @for ($i = 0; $i < 6; $i++)
+                <x-ui.card-article size="w-[300px]"
+                    title="Judul Artikel Kamu Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam velit praesentium dolores aliquam similique unde. Corrupti quo assumenda cum incidunt consequuntur earum. Veritatis!"
+                    description="Ini deskripsi singkat dari artikel yang kamu tampilkan. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam velit praesentium dolores aliquam similique unde. Corrupti quo assumenda cum incidunt consequuntur earum. Veritatis!"
+                    image="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png"
+                    link="/detail-artikel" />
+            @endfor
+        </div>
+    </div>
+</aside>
