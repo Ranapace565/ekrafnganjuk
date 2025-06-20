6655db39a9fb2a253865b1ea0b574357dd1b6866
Ranapace565 <Ranapace565@users.noreply.github.com>
2025-06-20 09:30:58 +0700

    update tampilan artikel

--- resources/views/main-visitor/article-detail.blade.php
+++ resources/views/main-visitor/article-detail.blade.php
@@ -1,11 +1,4 @@
 <x-layouts.visitor>
-    {{-- <x-slot:title>isi</x-slot:title> --}}
-    {{-- @include('main-visitor.article-detail.page1')
-
-    @include('main-visitor.article-detail.page2')
-
-    @include('main-visitor.article-detail.page3') --}}
-    {{-- @include('main-visitor.article-detail.page5') --}}
 
     <section class="bg-white dark:bg-gray-900 sm:px-6">
         <div class="grid max-w-screen-xl px-4 py-4 mx-auto lg:grid-cols-12">
--- resources/views/main-visitor/article.blade.php
+++ resources/views/main-visitor/article.blade.php
@@ -2,7 +2,4 @@
     {{-- <x-slot:title>isi</x-slot:title> --}}
     @include('main-visitor.article.page1')
 
-    {{-- @include('main-visitor.article.page2')
-
-    @include('main-visitor.article.page3') --}}
 </x-layouts.visitor>
